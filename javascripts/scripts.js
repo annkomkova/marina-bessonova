cfefc198676f5d09180c710bb8569c83c28bac03
Anna Komkova <ann.komkova98@gmail.com>
2025-06-25 19:09:20 +0300

    upd photobook

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
   showOverlay()
   showPropusk()
+  beuys1()
+  showArticlesAndExhibitions()
 })
+
+function showArticlesAndExhibitions() {
+  const articlesNav = document.querySelector('#articles')
+  const articles = document.querySelector('.articles')
+  const exhibitionsNav = document.querySelector('#exhibitions')
+  const exhibitions = document.querySelector('.exhibitions')
+
+  articlesNav.addEventListener('click', () => {
+    articles.classList.remove('none')
+    exhibitions.classList.add('none')
+  })
+  exhibitionsNav.addEventListener('click', () => {
+    exhibitions.classList.remove('none')
+    articles.classList.add('none')
+  })
+}
+
+function beuys1() {
+  const image = document.querySelector('.beuys1')
+  const text = document.querySelector('.beuys1 > div')
+  let clck = 1
+
+  image.addEventListener('click', () => {
+    clck++
+    text.classList.toggle('none')
+
+    if (clck % 2 == 0) {
+      image.style.backgroundImage = 'none'
+    } else {
+      image.style.backgroundImage = 'url("images/arts/beuys1.jpg")'
+    }
+  })
+}
 
 function showPropusk() {
   const block = document.querySelector('.propusk')
